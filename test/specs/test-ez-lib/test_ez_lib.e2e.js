7f4c02688ab46dfb3e30ff797126a69a48fc6add
alamhamim@pm.me <sarthok.nextt100@gmail.com>
2022-12-31 14:18:18 -0500

    more unit test added

--- test/specs/test-ez-lib/test_ez_lib.e2e.js
+++ test/specs/test-ez-lib/test_ez_lib.e2e.js
@@ -1,11 +1,25 @@
 var driver = require("@ez-library/wdio");
+const NOPNavigate = require("../../e2e/lib/NOPNavigate");
 
 describe("Test EZ Library", function(){
 
-    it("Test 1", async function(){
+    it("Test EZ Library: Testing goTo(), byId, element(), selectDDFeildByCss_BYINDEX():::", async function(){
         await driver.goTo("https://admin-demo.nopcommerce.com/login");
-        await (await driver.element("input[id='Email']")).setValue("admin@yourstore.com");
-        await (await driver.element("input[id='Password']")).setValue("admin");
-        await (await driver.element("button[type='submit']")).click();
+        await driver.byId("Email").setValue("admin@yourstore.com");
+        await driver.byId("Password").setValue("admin");
+        await driver.element("button[type='submit']").click();
+        await NOPNavigate.navigateTo("Catalog", "Products");
+        await driver.selectDDFeildByCss_BYINDEX("select[id='SearchCategoryId']", 1);
+        await browser.pause(5000);
+    })
+
+    it("Test EZ Library: Testing selectDDFeildByCss_BYTEXT():::", async function(){
+        await driver.goTo("https://admin-demo.nopcommerce.com/login");
+        await driver.byId("Email").setValue("admin@yourstore.com");
+        await driver.byId("Password").setValue("admin");
+        await driver.element("button[type='submit']").click();
+        await NOPNavigate.navigateTo("Catalog", "Products");
+        await driver.selectDDFieldByCss_BYTEXT("select[id='SearchCategoryId']", "Books");
+        await browser.pause(5000);
     })
 })
